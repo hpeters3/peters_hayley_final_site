343423230641fa91d4ddedff1ca75355bf82f84b
hpeters3 <hpeters3@academic.rrc.ca>
2025-08-13 18:29:44 -0500

    Building Blocks

---
+++ src/data.js
@@ -0,0 +1,26 @@
+export const projects = [
+  {
+    title: "Parallel Reads",
+    subtitle: "A fully development PHP webiste.",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "#",
+    link: "https://parallelreads.com",
+  },
+  {
+    title: "Zelda API Manager",
+    subtitle: "Using Ruby",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "#",
+    link: "#",
+  },
+  {
+    title: "Mockup",
+    subtitle: "HTML & CSS",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "#",
+    link: "#",
+  },
+];
